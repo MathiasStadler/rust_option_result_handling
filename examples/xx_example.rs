48637754f77e9b244b2ecefba2e4cc9431c63f0b
Mathias <email@example.com>
2024-04-20 14:04:19 +0200

    int

---
+++ examples/xx_example.rs
@@ -0,0 +1,23 @@
+ fn main() {
+    println!("Hello, example!");
+ }
+/*
+export FILE_NAME=XX_example.rs
+export FILE_DIR_NAME=examples
+git add $FILE_DIR_NAME/$FILE_NAME
+git commit --all --message="-> Add BEFORE housekeeping => $FILE_DIR_NAME/$FILE_NAME"
+git push
+# cargo install --list
+# cargo update --workspace
+cargo clippy --fix
+cargo clippy --fix --examples
+# cargo check --verbose
+# cargo check --verbose --examples
+cargo check
+cargo check --examples
+cargo fmt -- --emit=files
+git commit --all --message="-> Add AFTER housekeeping => $FILE_DIR_NAME/$FILE_NAME"
+git push
+cargo run --example $(echo $FILE_DIR_NAME/$FILE_NAME | cut -d . -f 1)
+echo "ReturnCode => $?"
+*/
